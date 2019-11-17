7aaf53529bddfe98fa9e058f8d4c2a4d475b08f7
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2019-11-17 20:56:14 +0100

    Added fulscreen support

--- main.c
+++ main.c
@@ -1,35 +1,86 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-const int WIDTH = 800;
-const int HEIGHT = 600;
+int windowedWidth = 1280;
+int windowedHeight = 720;
+int fullscreenWidth = 1920;
+int fullscreenHeight = 1080;
+
+
+void draw(SDL_Renderer* renderer)
+{
+    
+}
+
+
+void toggleFullscreen(SDL_Window* window)
+{
+    // https://stackoverflow.com/questions/30629106/sdl2-how-to-properly-toggle-fullscreen
+
+    Uint32 fullscreenFlag = SDL_WINDOW_FULLSCREEN;
+    int isFullscreen = SDL_GetWindowFlags(window) & fullscreenFlag;
+    SDL_SetWindowFullscreen(window, isFullscreen ? 0 : fullscreenFlag);
+    SDL_SetWindowSize(window, isFullscreen ? windowedWidth : fullscreenWidth, isFullscreen ? windowedHeight : fullscreenHeight);
+    // SDL_ShowCursor(isFullscreen);
+}
+
 
 int main (int argc, char* args[])
 {
     SDL_Window* window = NULL;
-    SDL_Surface* renderSurface = NULL;
+    SDL_Renderer* renderer = NULL;
 
+    // SDL2 and Window intialization
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("ERROR: Couldn't initialize SDL2\n");
-        return -1;
+        return 0;
     }
 
-    window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN );
+    window = SDL_CreateWindow("Times Tables", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowedWidth, windowedHeight, SDL_WINDOW_SHOWN);
 
     if (window == NULL)
     {
         printf("ERROR: Couldn't create the window");
-        return -1;
+        return 0;
     }
 
-    renderSurface = SDL_GetWindowSurface(window);
+    renderer = SDL_CreateRenderer(window, -1, 0);
 
-    SDL_FillRect(renderSurface, NULL, SDL_MapRGB(renderSurface->format, 0xFF, 0xFF, 0xFF));
-    
-    SDL_UpdateWindowSurface(window);
+    int quit = 0;
 
-    SDL_Delay(10000);
+    while (!quit)
+    {
+        // Event handling
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    quit = 1;
+                    break;
+                
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_F1) toggleFullscreen(window); break;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // Clearing window
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        
+        // Drawing
+        draw(renderer);
+
+        // Updating the window
+        SDL_RenderPresent(renderer);
+    }
 
     SDL_DestroyWindow(window);
 

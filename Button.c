065e7a52fc9c60f94574d7f40a032f9ea83b30e3
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2019-12-17 19:53:01 +0100

    Refactored buttons, with text now

--- Button.c
+++ Button.c
@@ -1,36 +1,66 @@
 #include "Button.h"
 
 #include "App.h"
+#include "SDL2/SDL_ttf.h"
 
-struct Button CreateButton (
+void CreateButton (
+    struct App* app,
+    struct Button* b,
     int x_, int y_, int w_, int h_, 
-    char string_[50],
+    char* string_,
     SDL_Color borderColor_, 
-    SDL_Color fillColor_, SDL_Color hoverColor_, SDL_Color pressColor_
+    SDL_Color fillColor_, SDL_Color hoverColor_, SDL_Color pressColor_,
+    SDL_Color textColor_
     )
 {
-    struct Button t;
-    t.rect.x = x_;
-    t.rect.y = y_;
-    t.rect.w = w_;
-    t.rect.h = h_;
+    // Geometry
 
-    t.borderColor = borderColor_;
+    b->rect.x = x_;
+    b->rect.y = y_;
+    b->rect.w = w_;
+    b->rect.h = h_;
 
-    t.fillColor = fillColor_;
-    t.hoverColor = hoverColor_;
-    t.pressColor = pressColor_;
+    // Color
 
-    t.isPressed = 0;
+    b->borderColor = borderColor_;
 
-    strcpy(t.string, string_);
+    b->fillColor = fillColor_;
+    b->hoverColor = hoverColor_;
+    b->pressColor = pressColor_;
 
-    return t;
+    // Control
+
+    b->isPressed = 0;
+
+    // Text
+
+    TTF_Font* font = TTF_OpenFont("./font.ttf", 25);
+
+    if (!font)
+        printf("Error with font : %s\n", TTF_GetError());
+
+    b->textColor = textColor_;
+    SDL_Surface* surface = TTF_RenderText_Solid(font, string_, b->textColor);
+    b->textTexture = SDL_CreateTextureFromSurface(app->renderer, surface);
+
+    SDL_FreeSurface(surface);
+    TTF_CloseFont(font);
+
+    int texW = 0;
+    int texH = 0;
+    
+    SDL_QueryTexture(b->textTexture, NULL, NULL, &texW, &texH);
+    
+    b->textRect = malloc(sizeof(SDL_Rect));
+    b->textRect->w = texW;
+    b->textRect->h = texH;
+    b->textRect->x = b->rect.x + (b->rect.w / 2) - (b->textRect->w / 2);
+    b->textRect->y = b->rect.y + (b->rect.h / 2) - (b->textRect->h / 2);
 }
 
 void DrawButton (struct App* app, struct Button* button)
 {
-    // Callback function
+    // TODO: Callback function
 
     if (button->callCallback == 1)
     {
@@ -39,7 +69,7 @@ void DrawButton (struct App* app, struct Button* button)
     }
 
     // Drawing
-
+    
     SDL_Color* bc = &(button->borderColor);
     SDL_Color* fc = &(button->fillColor);
     SDL_Color* hc = &(button->hoverColor);
@@ -62,6 +92,11 @@ void DrawButton (struct App* app, struct Button* button)
 
     SDL_SetRenderDrawColor(app->renderer, bc->r, bc->g, bc->b, bc->a);
     SDL_RenderDrawRect(app->renderer, &(button->rect));
+
+    // Drawing text
+
+    SDL_RenderCopy(app->renderer, button->textTexture, NULL, button->textRect);
+    SDL_RenderPresent(app->renderer);
 }
 
 static int IsPointInRect(Sint32 x, Sint32 y, SDL_Rect* rect)
@@ -101,4 +136,36 @@ void EventButton (struct Button* button, SDL_Event* e)
     {
         button->isPressed = 0;
     }
+}
+
+void ChangeTextButton (struct App* app, struct Button* b, char* newText)
+{
+    TTF_Font* font = TTF_OpenFont("./font.ttf", 25);
+
+    if (!font)
+        printf("Error with font : %s\n", TTF_GetError());
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, newText, b->textColor);
+    b->textTexture = SDL_CreateTextureFromSurface(app->renderer, surface);
+
+    SDL_FreeSurface(surface);
+    TTF_CloseFont(font);
+
+    int texW = 0;
+    int texH = 0;
+    
+    SDL_QueryTexture(b->textTexture, NULL, NULL, &texW, &texH);
+    
+    b->textRect = malloc(sizeof(SDL_Rect));
+    b->textRect->w = texW;
+    b->textRect->h = texH;
+    b->textRect->x = b->rect.x + (b->rect.w / 2) - (b->textRect->w / 2);
+    b->textRect->y = b->rect.y + (b->rect.h / 2) - (b->textRect->h / 2);
+}
+
+void DestroyButton (struct Button* button)
+{
+    SDL_DestroyTexture(button->textTexture);
+
+    free(button->textRect);
 }
--- Button.h
+++ Button.h
@@ -5,28 +5,44 @@
 
 struct Button
 {
+    // Geometry
+
     SDL_Rect rect;
+
+    // Color
+
     SDL_Color borderColor;
     SDL_Color fillColor;
     SDL_Color hoverColor;
     SDL_Color pressColor;
+    SDL_Color textColor;
+
+    // Control
 
     int isHovered;
     int isPressed;
     int callCallback;
 
-    char string[50];
+    // Text
+
+    SDL_Texture* textTexture;
+    SDL_Rect* textRect;
 };
 
 struct App;
 
-struct Button CreateButton (
+void CreateButton (
+    struct App* app,
+    struct Button* button,
     int x_, int y_, int w_, int h_, 
-    char string_[50],
+    char* string_,
     SDL_Color borderColor_, 
-    SDL_Color fillColor_, SDL_Color hoverColor_, SDL_Color pressColor
+    SDL_Color fillColor_, SDL_Color hoverColor_, SDL_Color pressColor,
+    SDL_Color textColor_
     );
 void DrawButton (struct App* app, struct Button* button);
 void EventButton (struct Button* button, SDL_Event* event);
+void ChangeTextButton (struct App* app, struct Button* b, char* newText);
+void DestroyButton (struct Button* button);
 
 #endif
